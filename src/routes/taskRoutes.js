dc1b984566ed63e6ec0ff0827a26ef84126dfe8e
Landon Motley <lmotley1@charlotte.edu>
2025-10-28 23:10:06 -0400

    implement getTaskById and update docs

--- src/routes/taskRoutes.js
+++ src/routes/taskRoutes.js
@@ -7,4 +7,6 @@ const router = express.Router();
 router.get('/', taskController.getTasks);
 router.post('/', validateTask, taskController.createTask);
 
+router.get('/:id', taskController.getTaskById);
+
 export default router;
--- src/services/taskService.js
+++ src/services/taskService.js
@@ -1,4 +1,5 @@
 import * as taskRepository from '../repositories/taskRepo.js';
+import prisma from '../config/db.js';
 
 export async function getAllTasks() {
   return taskRepository.findAll();
@@ -6,4 +7,8 @@ export async function getAllTasks() {
 
 export async function createTask(newTask) {
   return taskRepository.create(newTask);
+}
+
+export async function getTaskById(id) {
+  return taskRepository.findById(id);
 }
